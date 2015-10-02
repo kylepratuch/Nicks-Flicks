868f0a4e9f6712b9250a713dec25e9b6872f4582
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-10-02 10:08:07 -0700

    add test view for reviews

--- app.js
+++ app.js
@@ -7,10 +7,24 @@ nicksFlicks.config(function($stateProvider, $urlRouterProvider) {
     views: {
       'header':{
         templateUrl: "partials/header.html",
-        controller: 'HeadersCtrl',
+        controller: "HeadersCtrl",
       },
       'body': {
         templateUrl: "partials/home.html",
+      },
+    }
+  });
+
+  $stateProvider.state('reviews', {
+    url:"",
+    views: {
+      'header':{
+        templateUrl: "partials/header.html",
+        controller: "HeadersCtrl",
+      },
+      'body':{
+        templateUrl: "partials/reviews.html",
+        controller: "ReviewsCtrl",
       },
     }
   });

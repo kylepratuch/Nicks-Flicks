14476692b4ce1f8edd188c4ba0889aeaa86b909d
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-10-02 16:58:16 -0700

    commit before submit: unfinished

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -1,7 +1,7 @@
-nicksFlicks.directive("nightMode", function() {
-  return function(scope, element, attrs) {
-    element.bind("click",function() {
-      element.toggleClass(attrs.nightMode);
-    });
-  }
-});
+// nicksFlicks.directive("nightMode", function() {
+//   return function(scope, element, attrs) {
+//     element.bind("click",function() {
+//       document.documentElement.toggleClass(attrs.nightMode);
+//     });
+//   }
+// });
